56411410023f81d361302077d0e1b782bc58edb6
thebrahma <utpalbrahma1995@gmail.com>
2018-08-14 12:23:38 +0530

    directory

---
+++ URL/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/thebrahma/Documents/python/git/shortening-service/URL shortening service/URL/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
